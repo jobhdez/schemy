6dd3fd9c67d6775b695b2a4591bad51ab413efb7
Job Hernandez Lara <lara2993@proton.me>
2025-01-24 10:10:28 -0800

    support application

--- ToAnf.hs
+++ ToAnf.hs
@@ -17,7 +17,7 @@ toanf' :: Exp -> Int ->  Exp
 
 toanf' (If (Prim op e e2) thn els) n =
   let tmp = "tmp_" ++ show n in
-    desugar (Let [Binding (Varexp (Var tmp)) (Prim op e e2)] (If (Varexp (Var tmp)) (toanf thn) (toanf els)))
+    desugar(Let [Binding (Varexp (Var tmp)) (Prim op e e2)] (If (Varexp (Var tmp)) (toanf thn) (toanf els)))
 
 toanf' (If cnd thn els) n =
   case cnd of
@@ -32,10 +32,32 @@ toanf' (If cnd thn els) n =
             _  -> toanf' cnd' n
          
 
-
 toanf' (Application exps) n =
-  Application (map (\x -> (toanf' x n)) exps)
-
+  let op = head exps in
+    let (x:xs) = tail exps in
+      toanf'' op n (x:xs)
 
 toanf' exp n =
   exp
+
+toanf'' :: Exp -> Int -> [Exp] -> Exp
+toanf'' op n (x:xs) =
+  case x of
+    If cnd' thn' els' ->
+      let tmp = "tmp_" ++ show n
+          tmp2 = "tmp_" ++ show (n + 1)
+      in case cnd' of
+           Bool cnd'' ->
+             let exps = (map (\x -> toanf' x n) xs) in
+               desugar
+               (Let [Binding (Varexp (Var tmp)) (If cnd' (toanf' thn' n) (toanf' els' n))] (Application (op : exps)))
+                 
+                 
+           Prim op' e e2 ->
+             let exps = (map (\x -> toanf' x n) xs) in
+               desugar
+               (Let [ Binding (Varexp (Var tmp)) (Prim op' e e2)] (Let [Binding (Varexp (Var tmp2)) (If (Varexp (Var tmp)) (toanf' thn' n) (toanf' els' n))] (Application (op : exps))))
+           _ -> toanf'' op (n + 1) xs
+    _ ->
+      let exps = (map (\x -> toanf' x n) xs) in
+        Application ([op] ++ exps)
