73518da38f521ad9da9b3c7973ab7d0b7ed8b70d
Job Hernandez <lara2993@proton.me>
2025-01-27 15:01:07 -0800

    update application node

--- ClosureConversion.hs
+++ ClosureConversion.hs
@@ -1,7 +1,7 @@
 module ClosureConversion where
 
 import Parser (
-   Exp(Application, Prim, Lambda, DefineProc, If, Set, Varexp, Let, Int, Closure),
+   Exp(Application, Prim, Lambda, DefineProc, If, Set, Varexp, Let, Int, Closure, Tuple, TupleRef),
    Var(Var),
    Binding(Binding),
    lexer,
@@ -35,7 +35,7 @@ closure (x:xs) env =
 closure' :: Exp -> ClosureEnv -> Int -> ([Exp], ClosureEnv)
 closure' (DefineProc var params exp) env n =
   case exp of
-    Application (op:xs) ->
+    Application op exps ->
       case op of
         Lambda var' (Lambda var'' exp') ->
           let (clsr, env') = closure' op env n
@@ -50,17 +50,19 @@ closure' (DefineProc var params exp) env n =
                           (Varexp (Var ("lamb_" ++ show n)))
                           (Int (length (getLamVars (head clsr))))
                           fvs--}
-              lambName = getClosureName (head clsr)
+              lamName = getClosureName (head clsr)
               lamenv = lambdaEnv env'
               --lamEnv = Map.insert (Var lambName) lambdaexp (lambdaEnv env')
               def = clsr
-              (exps, env''') = closure xs (ClosureEnv { fvEnv = Map.empty, lambdaEnv = Map.empty })
-              ops = [Varexp (Var "tupleref"), (Varexp lambName), Int 0] ++ exps
-              app = (Application ops)
+              (exps', env''') = closure exps (ClosureEnv { fvEnv = Map.empty, lambdaEnv = Map.empty })
+              --op = TupleRef (Varexp lamName) n
+              app = (Application (Varexp lamName) exps')
           in (def ++ [app], ClosureEnv {fvEnv = freevars, lambdaEnv = lamenv})
-        _ ->
-          closure' (Application (op:xs)) env n
-
+    _->
+      ([exp], env)
+          
+         
+          
 closure' (Lambda vars exp) env n =
   case exp of
     (Lambda vars' exp') ->
@@ -70,17 +72,17 @@ closure' (Lambda vars exp) env n =
             let fvs = freeVariables vars' exp' in
               let fvsEnv' = Map.insert (Var ("fvs_" ++ show n)) fvs (fvEnv v) in
                 let lamEnv' = Map.insert (Var ("lamb_" ++ show n)) (Lambda vars' exp') (lambdaEnv v) in
-                  ([DefineProc (Var ("lamb_" ++ show n)) ([Var ("fvs_" ++ show n)] ++ vars) (Closure (Int (length vars')) (Varexp (Var ("lamb_" ++ show (n+1)))) (Int (length vars'')) fvs), (DefineProc (Var ("lamb_" ++ (show (n+1)))) ([Var ("fvs_" ++ show n)] ++ vars') (makeClosure'Exp exp' fvs (Var ("fvs_" ++ show n))))], (ClosureEnv { fvEnv = fvsEnv', lambdaEnv = lamEnv' }))
+                  ([DefineProc (Var ("lamb_" ++ show n)) ([Var ("fvs_" ++ show n)] ++ vars) (Closure (Int (length vars')) (Varexp (Var ("lamb_" ++ show (n+1)))) (Int (length vars'')) fvs), (DefineProc (Var ("lamb_" ++ (show (n+1)))) ([Var ("fvs_" ++ show n)] ++ vars') (makeLets exp' fvs (Var ("fvs_" ++ show n)) 0))], (ClosureEnv { fvEnv = fvsEnv', lambdaEnv = lamEnv' }))
     _ ->
       let freeVars = freeVariables vars exp
           lamName = ("lambda_" ++ show n)
           fvName = Var ("fv_" ++ show n)
           fvEnv' = Map.insert fvName freeVars (fvEnv env)
           lambdaEnv' = Map.insert (Var lamName) (Lambda vars exp) (lambdaEnv env)
-          exp' = makeClosure'Exp exp freeVars fvName
+          exp' = makeLets exp freeVars fvName 0
           clsr = Closure (Int (length vars)) (Varexp (Var ("lamb_" ++ show n))) (Int (length vars)) freeVars
       in
-        ([DefineProc (Var lamName) ([fvName] ++ vars) (makeClosure'Exp exp freeVars fvName)], ClosureEnv { fvEnv = fvEnv', lambdaEnv = lambdaEnv' })
+        ([DefineProc (Var lamName) ([fvName] ++ vars) (makeLets exp freeVars fvName 0)], ClosureEnv { fvEnv = fvEnv', lambdaEnv = lambdaEnv' })
        
 
 closure' (If cnd thn els) env n =
@@ -93,27 +95,22 @@ closure' (Set var e) env n =
   let (e', env') = closure' e env n
   in ([Set var (head e')], env') 
 
-closure' (Application (x:xs)) env n =
-  case x of
-    Varexp op ->
-      if Map.member op (fvEnv env)
-      then
-        let tupName = Var ("tup_" ++ show n)
-            tup = Application (Varexp (Var "tuple") : xs)
-        in 
-          let (tup', env') = closure' tup env n
-              (app', env'') = closure' (Application (Varexp op : Varexp tupName : xs)) env' n
-          in ([Let [Binding (Varexp tupName) (head tup')] (head app')], env'')
-      else 
-        let (xs', env') = closure' (Application xs) env n
-        in ([Application (x : xs')], env')
+closure' (Application op exps) env n =
+  case op of
+    Lambda vars exp ->
+      let (opclsr, env') = closure' op env n in
+        let lamName = getDefprocName (head opclsr) in
+          let operands = exps in
+            (opclsr ++  [(Application (Varexp lamName) operands)], env')
+    (Varexp (Var v)) ->
+      ([Application (Varexp (Var v)) exps], env)
       
 closure' exp env _ = ([exp], env)  
 
-makeClosure'Exp :: Exp -> [Var] -> Var -> Exp
-makeClosure'Exp exp [] _ = exp
-makeClosure'Exp exp (x:xs) env =
-  Let [Binding (Varexp x) (Application [Varexp (Var "tupleref"), Varexp env, Varexp x])] (makeClosure'Exp exp xs env)
+makeLets :: Exp -> [Var] -> Var -> Int -> Exp
+makeLets exp [] _ n = exp
+makeLets exp (x:xs) env n =
+  Let [Binding (Varexp x) (TupleRef (Varexp env) (Int n))] (makeLets exp xs env (n+1))
 
 freeVariables :: [Var] -> Exp -> [Var]
 freeVariables vars (Set (Varexp (Var varName)) exp) =
@@ -125,9 +122,11 @@ freeVariables vars (Prim _ (Varexp (Var e1)) (Varexp (Var e2))) =
   in fv1 ++ fv2
 
 freeVariables vars (Prim _ (Varexp (Var e1)) e2) =
-  let fv1 = if Var e1 `elem` vars then [] else freeVariables vars e2
-      fv2 = freeVariables vars e2
-  in fv1 ++ fv2
+  let fvs = freeVariables vars e2
+      fvs2 = if Var e1 `elem` vars then [] else [Var e1]
+  in
+    fvs ++ fvs2
+                    
   
 freeVariables vars (If cnd thn els) =
   freeVariables vars cnd ++ freeVariables vars thn ++ freeVariables vars els
@@ -155,3 +154,6 @@ getLamExp (Lambda vars exp) = exp
 getClosureName :: Exp -> Var
 getClosureName (DefineProc var vars (Closure n (Varexp (Var v)) n2 fvs)) =
   Var v
+
+getDefprocName :: Exp -> Var
+getDefprocName (DefineProc nameVar vars e) = nameVar
