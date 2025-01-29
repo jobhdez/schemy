e9ec1ef69e006efb850c8b41c3ecd80ed5d13d7f
Job Hernandez <lara2993@proton.me>
2025-01-28 19:29:36 -0800

    read from file

--- Compile.hs
+++ Compile.hs
@@ -1,31 +1,26 @@
 module Compile where
 import System.IO
 import Parser (
-  Exp,
+  Exp(DefineProc),
   lexer,
   toAst
   )
 import Desugar (desugar)
 import ToAnf (toAnf)
-import ClosureConversion (closureConversion)
+import ClosureConversion (closure)
 import ToSelect (
   Instruction,
   toselect'
   )
 
--- Compile function that desugars, converts to ANF, performs closure conversion, and then selects instructions.
-compile :: String -> [Instruction]
+compile :: String -> IO ()
 compile exp =
   let ast = toAst (lexer exp)
-      anf = toAnf (desugar ast)
-      (closure, _) = closureConversion anf  -- Ignore the environment if it's not needed
-  in toselect' closure 0
+      anf = toAnf ast
+      closr = closure anf 0
+  in print closr
 
--- Main function for reading a file and compiling it.
-main :: IO ()
 main = do
-    putStrLn "Enter the file path:"
-    filePath <- getLine  -- Get the file path from the user
-    fileContent <- readFile filePath  -- Read the file content
-    let instructions = compile fileContent  -- Compile the content
-    print instructions  -- Print the instructions
+  s <- readFile "Test.scm"
+  compile s
+
