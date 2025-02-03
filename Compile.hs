9770dead71e025d3805f0e598e42fbbafe365fcf
Job Hernandez Lara <lara2993@proton.me>
2025-02-02 17:49:31 -0800

    add new file

--- Compile.hs
+++ Compile.hs
@@ -8,19 +8,16 @@ import Parser (
 import Desugar (desugar)
 import ToAnf (toAnf)
 import ClosureConversion (closure)
-import ToSelect (
-  Instruction,
-  toselect'
-  )
+
 
 compile :: String -> IO ()
 compile exp =
   let ast = toAst (lexer exp)
-      anf = toAnf ast
+      anf = toAnf (desugar ast)
       closr = closure anf 0
   in print closr
 
 main = do
-  s <- readFile "Test.scm"
+  s <- readFile "testx.scm"
   compile s
 
